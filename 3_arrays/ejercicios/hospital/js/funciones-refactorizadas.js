5d153561ddda217b544fc035e626014e76134436
JAVI-LAPTOP\Admin <jav.fer.dev@gmail.com>
2023-05-25 16:46:19 +0200

    refactorizado hospital y visto indexof

--- 3_arrays/ejercicios/hospital/js/funciones-refactorizadas.js
+++ 3_arrays/ejercicios/hospital/js/funciones-refactorizadas.js
@@ -1,11 +1,12 @@
 //mostrar lista de pacientes (solamente mostrar la lista de pacientes completa)
-const listaPacientes = [];
-function mostrarListaPacientes(pListaPacientes) {
-  for (let paciente of pListaPacientes) {
-    listaPacientes[listaPacientes.length] = paciente;
-  }
-  return listaPacientes;
-}
+
+// const listaPacientes = [];
+// function mostrarListaPacientes(pListaPacientes) {
+//   for (let paciente of pListaPacientes) {
+//     listaPacientes[listaPacientes.length] = paciente;
+//   }
+//   return listaPacientes;
+// }
 
 
 // Sacar pacientes con gripe
@@ -34,15 +35,23 @@ let resultado = filterPatientsByDiagnosis(pacientes, 'gripe')
  *recibe tres parametros lista de pacientes en json, y edad minima y edad maxima en numero
  * devuelve una lista de pacientes en formato json
  */
-function filterPatientsByAge(pPatientList, pAgeMin, pAgeMax) {
-  const listaFiltrada = [];
-  for (let paciente of pPatientList) {
-    if (paciente.edad >= pAgeMin && paciente.edad <= pAgeMax) {
-      listaFiltrada[listaFiltrada.length] = paciente;
-    }
-  }
-  return listaFiltrada;
-}
+
+// function filterPatientsByAge(pPatientList, pAgeMin, pAgeMax) {
+//   const listaFiltrada = [];
+//   for (let paciente of pPatientList) {
+//     if (paciente.edad >= pAgeMin && paciente.edad <= pAgeMax) {
+//       listaFiltrada[listaFiltrada.length] = paciente;
+//     }
+//   }
+//   return listaFiltrada;
+//}
+//refactorizado
+const filterPatientsByAge = (pPatientList, pAgeMin, pAgeMax) => pPatientList.filter(paciente => paciente.edad >= pAgeMin && paciente.edad <= pAgeMax)
+let resultado2 = filterPatientsByAge(pacientes, 18, 25)
+// console.log(resultado2)
+
+
+
 
 // sacar pacientes cuyo numero de la ss acabe en 0
 
@@ -50,14 +59,22 @@ function filterPatientsByAge(pPatientList, pAgeMin, pAgeMax) {
  *recibe dos parametros lista de pacientes en json, y letra o numero en string de búsqueda
  * devuelve una lista de pacientes en formato json
  */
-function filterPatientsBySs(pPatientList, pLetter) {
-  const listaFiltrada = [];
-  for (let paciente of pPatientList) {
-    let numero = paciente.numeroSS;
-    let ultimoNumero = numero % 10; // Si fuese STRING, lo haria con numero.length -1, numero%100 saco los dos últimos digitos
-    if (ultimoNumero === pLetter) {
-        listaFiltrada[listaFiltrada.length] = paciente;
-    }
-  }
-return listaFiltrada
-}
+
+// function filterPatientsBySs(pPatientList, pLetter) {
+//   const listaFiltrada = [];
+//   for (let paciente of pPatientList) {
+//     let numero = paciente.numeroSS;
+//     let ultimoNumero = numero % 10; // Si fuese STRING, lo haria con numero.length -1, numero%100 saco los dos últimos digitos
+//     if (ultimoNumero === pLetter) {
+//         listaFiltrada[listaFiltrada.length] = paciente;
+//     }
+//   }
+// return listaFiltrada
+// }
+
+
+
+//refactorizado
+const filterPatientsBySs =(pPatientList, pLetter) => pPatientList.filter(paciente => paciente.numeroSS%10 === pLetter)
+let resultado3 = filterPatientsBySs(pacientes, 0)
+
--- 3_arrays/ejercicios/hospital/js/main.js
+++ 3_arrays/ejercicios/hospital/js/main.js
@@ -1,13 +1,13 @@
 //Ejecución de pacientes filtrados por enfermedad
-const filterPatientsByDiagnosis= filterPatientsByAge(listaPacientes, 'gripe')
-console.log(filterPatientsByDiagnosis(listaPacientes, 'gripe'))
-console.log(filterPatientsByDiagnosis(listaPacientes, 'diabetes'))
+// const filterPatientsByDiagnosis= filterPatientsByAge(listaPacientes, 'gripe')
+console.log(filterPatientsByDiagnosis(pacientes, 'gripe'))
+
 
 //Ejecución de pacientes filtrados por edad
- const filterPatientsByAge = filterPatientsByAge(listaPacientes, '18', '35')
-console.log(filterPatientsByAge(listaPacientes, '18', '35'))
+//  const filterPatientsByAge = filterPatientsByAge(listaPacientes, '18', '35')
+console.log(filterPatientsByAge(pacientes, '18', '35'))
 
 
 //Ejecución de pacientes filtrados por terminación de número de SS
-const filterPatientsBySs = filterPatientsBySs(pacientes, 0)
+// const filterPatientsBySs = filterPatientsBySs(pacientes, 0)
 console.log(filterPatientsBySs(pacientes, 0))
